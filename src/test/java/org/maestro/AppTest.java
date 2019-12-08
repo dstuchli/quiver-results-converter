d1a12b084c51d64d7c2c606c84c2ba3ad9af9367
Dominik Stuchlik <dstuchli@redhat.com>
2019-12-08 10:48:41 +0100

    NEW: change to Maven project
    ~~
    Note:
        New maven project was generated with pom.xml. Minor fixes were
        applied to project to work properly.
    ~~

---
+++ src/test/java/org/maestro/AppTest.java
@@ -0,0 +1,20 @@
+package org.maestro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
